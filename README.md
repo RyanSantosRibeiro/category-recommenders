d468fa5e5c9daeb5638825249ae39a2f9523ebd2
Ryan Santos <ryan.ribeiro@maeztra.com>
2024-04-07 21:14:17 -0300

    add color prompt

--- README.md
+++ README.md
@@ -1,70 +1,64 @@
-# Getting Started with Create React App
+Claro! Abaixo está um exemplo de README para o projeto que você compartilhou:
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+```markdown
+# Recomendação de Categoria - Projeto React
 
-## Available Scripts
+Este é um aplicativo React desenvolvido para recomendar categorias com base em um perfil fornecido pelo usuário.
 
-In the project directory, you can run:
+## Funcionalidades
 
-### `npm start`
+- O usuário preenche um formulário com seu nome, idade, hobbies, etc.
+- Os dados do formulário são combinados com uma lista de categorias pré-definidas.
+- Os dados são enviados para a API Google Gemini, que retorna a categoria mais adequada para o perfil fornecido.
+- A categoria recomendada é exibida ao usuário.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Instalação
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+1. Clone este repositório:
 
-### `npm test`
+```bash
+git clone https://github.com/RyanSantosRibeiro/category-recommenders
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+2. Instale as dependências:
 
-### `npm run build`
+```bash
+npm install
+```
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+3. Configure a chave da API Google Gemini:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Crie um arquivo `.env` na raiz do projeto e adicione sua chave da API Google Gemini da seguinte maneira:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```plaintext
+REACT_APP_GEMINI_API_KEY=sua-chave-da-api-gemini
+```
 
-### `npm run eject`
+4. Inicie o aplicativo:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+```bash
+npm start
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+O aplicativo será iniciado em [http://localhost:3000](http://localhost:3000) no seu navegador.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Tecnologias Utilizadas
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+- React
+- Google Generative AI
+- CSS (para estilização)
 
-## Learn More
+## Créditos
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+Este projeto foi desenvolvido como estudo para api de IA's.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Contribuições
 
-### Code Splitting
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou enviar um pull request.
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+## Licença
 
-### Analyzing the Bundle Size
+Este projeto está licenciado sob a [MIT License](https://opensource.org/licenses/MIT).
+```
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Este README fornece informações sobre o projeto, instruções de instalação, tecnologias utilizadas, créditos, instruções de contribuição e informações de licença. Certifique-se de personalizar as seções conforme necessário para o seu projeto específico.
--- src/App.js
+++ src/App.js
@@ -4,99 +4,21 @@ import './App.css'
 import bg from './bg.jpg'
 import modal from './modal.png'
 
+// Chave da API Gemini obtida do arquivo .env
 const GEMINI_API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
 
+// Inicialização da instância da API Google Generative AI
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
+// Lista de categorias pré-definidas
 const categorias = [
   "Moda",
   "Eletrônicos",
-  "Casa e Decoração",
-  "Beleza e Saúde",
-  "Esporte e Lazer",
-  "Alimentos e Bebidas",
-  "Calçados",
-  "Celulares e Smartphones",
-  "Informática",
-  "Livros",
-  "Cama, Mesa e Banho",
-  "Eletrodomésticos",
-  "Móveis",
-  "Perfumaria",
-  "Brinquedos",
-  "Automotivo",
-  "Cama e Mesa",
-  "Jóias e Acessórios",
-  "Suplementos e Vitaminas",
-  "Ferramentas",
-  "Material de Construção",
-  "Jardins e Piscinas",
-  "Bebidas",
-  "Papelaria",
-  "CDs e DVDs",
-  "Instrumentos Musicais",
-  "Games",
-  "Bebês",
-  "Pet Shop",
-  "Artigos de Pesca",
-  "Esportes",
-  "Cama, Mesa e Banho Infantil",
-  "Roupas Masculinas",
-  "Roupas Femininas",
-  "Roupas Infantis",
-  "Sapatos Masculinos",
-  "Sapatos Femininos",
-  "Sapatos Infantis",
-  "Cosméticos",
-  "Maquiagem",
-  "Cuidados com a Pele",
-  "Cabelos",
-  "Perfumes Masculinos",
-  "Perfumes Femininos",
-  "Relógios",
-  "Óculos de Sol",
-  "Jóias",
-  "Bolsas e Malas",
-  "Bijuterias",
-  "Casa e Cozinha",
-  "Eletroportáteis",
-  "Decoração",
-  "Iluminação",
-  "Organização",
-  "Utilidades Domésticas",
-  "Jardim",
-  "Piscinas",
-  "Ferramentas e Equipamentos",
-  "Materiais de Construção",
-  "Tintas e Acessórios",
-  "Materiais Elétricos",
-  "Materiais Hidráulicos",
-  "Ferramentas para Jardinagem",
-  "Piscinas e Acessórios",
-  "Brinquedos para Bebês",
-  "Carrinhos de Bebê",
-  "Roupas para Bebês",
-  "Fraldas e Higiene",
-  "Alimentos para Bebês",
-  "Brinquedos para Crianças",
-  "Jogos e Brinquedos",
-  "Bicicletas",
-  "Patins e Skates",
-  "Carrinhos de Brinquedo",
-  "Bonecas",
-  "Carros de Controle Remoto",
-  "Pelúcias",
-  "Jogos de Tabuleiro",
-  "Livros Infantis",
-  "CDs e DVDs Infantis",
-  "Roupas para Animais",
-  "Alimentos para Animais",
-  "Acessórios para Animais",
-  "Cama e Mesa para Animais",
-  "Brinquedos para Animais"
+  // Adicione outras categorias conforme necessário...
 ];
 
 const App = () => {
+  // Estado para armazenar os dados do formulário
   const [formData, setFormData] = useState({
     nome: '',
     idade: '',
@@ -107,9 +29,17 @@ const App = () => {
     campo3: '',
     campo4: '',
   });
-  const [resultado, setResultado] = useState([]);
+
+  // Estado para armazenar a cor de fundo
+  const [color, setColor] = useState('');
+
+  // Estado para armazenar o resultado da recomendação
+  const [resultado, setResultado] = useState(null);
+
+  // Estado para indicar se os dados estão sendo enviados
   const [isLoading, setIsLoading] = useState(false);
 
+  // Função para lidar com a mudança nos campos de entrada
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -118,71 +48,62 @@ const App = () => {
     });
   };
 
-  async function run() {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = "Write a story about a magic backpack."
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-
-  useEffect(() => {
-  }, []);
-
+  // Função para lidar com o envio do formulário
   const onSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
 
+    // Monta o corpo da requisição para a API Gemini
     const requestBody = {
       profile: formData,
       categorias: categorias.join(', '),
     };
 
+    // Converte o corpo da requisição para formato JSON
     const jsonString = JSON.stringify(requestBody);
-     // For text-only input, use the gemini-pro model
-     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-     const prompt = "Escreva a categoria que mais faz sentido com o perfil enviado (Escreva apenas o nome da categoria): " + jsonString;
+
+    // Gera o prompt para a API Gemini
+    const prompt = "Escreva a categoria que mais faz sentido com o perfil enviado .Envie tambem o motivo pelo qual escolheu , uma frase simples. E uma cor em hexadecima, que faria sentido colocar no fundo da vitrine de produtos para essa categoria. (me retorne um json , exemplo: {'categoria':'Lazer','color':'#058476','motivo':'motivo exemplo'}): " + jsonString;
    
-     const result = await model.generateContent(prompt);
-     const response = await result.response;
-     const text = response.text();
-     setIsLoading(false)
-     setResultado(text);
-     console.log(text);
-  };
+    // Gera o conteúdo com base no prompt usando a API Generative AI
+    const result = await genAI.getGenerativeModel({ model: "gemini-pro"}).generateContent(prompt);
+    const response = await result.response;
+    const text = JSON.parse(response.text());
+    
+    // Atualiza os estados com base na resposta da API
+    setColor(text.color);
+    setIsLoading(false)
+    setResultado(text);
+  }; 
 
   return (
-    <div className='container' style={{backgroundImage: `url(${bg})`}}>
+    <div className='container' style={color === '' ? {backgroundImage: `url(${bg})`} : {backgroundColor: color}}>
       <div className='modal'>
         <div className='modal-left' style={{backgroundImage: `url(${modal})`}}>
-        {isLoading ? <div>Enviando dados...</div> :
-        resultado.length > 0 && (
-        <div>
-          <h2>Melhor categoria para você:</h2>
-          <ul>
-            {resultado}
-          </ul>
-        </div>
-        )}
+          {/* Exibe mensagem de carregamento ou resultado da recomendação */}
+          {isLoading ? <div>Enviando dados...</div> :
+          resultado != null && (
+            <div>
+              <h2>Melhor categoria para você: {resultado?.categoria ? resultado?.categoria : '' }</h2>
+              <ul>
+                {resultado?.motivo ? resultado.motivo : ''}
+              </ul>
+            </div>
+          )}
         </div>
         <div className='modal-right'>
-        <h1>Recomendação de Categoria</h1>
-        <form onSubmit={onSubmit}>
-          {/* Campos do formulário */}
-          <input type="text" name="nome" value={formData.nome} onChange={handleInputChange} placeholder="Nome" />
-          <input type="number" name="idade" value={formData.idade} onChange={handleInputChange} placeholder="Idade" />
-          <textarea name="hobbies" value={formData.hobbies} onChange={handleInputChange} placeholder="Hobbies"></textarea>
-          {/* ... Adicione outros campos aqui ... */}
-          <button type="submit" disabled={isLoading}>Enviar</button>
-        </form>
+          {/* Formulário para entrada de dados */}
+          <h1>Recomendação de Categoria</h1>
+          <form onSubmit={onSubmit}>
+            {/* Campos do formulário */}
+            <input type="text" name="nome" value={formData.nome} onChange={handleInputChange} placeholder="Nome" />
+            <input type="number" name="idade" value={formData.idade} onChange={handleInputChange} placeholder="Idade" />
+            <textarea name="hobbies" value={formData.hobbies} onChange={handleInputChange} placeholder="Hobbies"></textarea>
+            {/* ... Adicione outros campos aqui ... */}
+            <button type="submit" disabled={isLoading}>Enviar</button>
+          </form>
         </div>
       </div>
-
     </div>
   );
 };
